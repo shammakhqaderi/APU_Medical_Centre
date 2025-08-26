3e530e387cc6aa8af415cc1a743aac20d3e5669d
SHAMMAKH AHMED AHMED QADERI <TP080605@mail.apu.edu.my>
2025-08-26 09:17:42 +0800

    new page for Login page

---
+++ src/main/java/com/myuni/apu_medical_centre/APU_Medical_Centre.java
@@ -0,0 +1,10 @@
+
+package com.myuni.apu_medical_centre;
+
+
+public class APU_Medical_Centre {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
